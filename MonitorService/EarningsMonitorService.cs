00cfa2a55b596d4a2ce0394fbd9868b8340852f0
Vladimir Glisic <vglisic@seatgeek.com>
2023-03-20 09:47:20 +0100

    result for the session

--- MonitorService/EarningsMonitorService.cs
+++ MonitorService/EarningsMonitorService.cs
@@ -1,15 +1,26 @@
 ﻿using HrServiceContract.Interfaces;
+using System;
 
 namespace MonitorService
 {
     public class EarningsMonitorService : IEarningsMonitorService
     {
         private IEmployeeService m_EmployeeService;
+        private DateTime m_now;
+
+        public EarningsMonitorService(IEmployeeService service, DateTime now)
+        {
+            m_EmployeeService = service;
+            m_now = now;
+        }
 
         public decimal GetOpenSessionEarnings(int employeeId)
         {
-            // todo: exercise
-            throw new System.NotImplementedException();
+            var contract = m_EmployeeService.GetContract(employeeId);
+            var incident = m_EmployeeService.GetOpenIncident(employeeId);
+
+            int duration = (int)(m_now - incident.StartTime).TotalMinutes;
+            return duration * contract.HourlyWage / 60;
         }
     }
 }
--- MyOnCallTimeTests/MonitorServiceTests.cs
+++ MyOnCallTimeTests/MonitorServiceTests.cs
@@ -1,18 +1,37 @@
+using HrServiceContract.Interfaces;
+using HrServiceContract.Model;
+using MonitorService;
+using Moq;
 using NUnit.Framework;
+using System;
 
 namespace MyOnCallTimeTests
 {
     public class MonitorServiceTests
     {
+        private const decimal HourlyWage = 500;
+        private readonly DateTime StartTime = new DateTime(2023, 1, 1, 9, 0, 0);
+
+        private Mock<IEmployeeService> m_employeeServiceMock;
+
         [SetUp]
         public void Setup()
         {
+            m_employeeServiceMock = new Mock<IEmployeeService>();
+            m_employeeServiceMock.Setup(x => x.GetContract(It.IsAny<int>())).Returns(new Contract { HourlyWage = HourlyWage });
         }
 
         [Test]
-        public void Test1()
+        public void GetOpenSessionEarnings_OneHour_OneHourWage()
         {
-            Assert.Fail();
+            var now = StartTime.AddHours(1);
+            m_employeeServiceMock.Setup(x => x.GetOpenIncident(It.IsAny<int>())).Returns(new Incident { StartTime = StartTime });
+
+            var service = new EarningsMonitorService(m_employeeServiceMock.Object, now);
+
+            decimal result = service.GetOpenSessionEarnings(It.IsAny<int>());
+
+            Assert.That(HourlyWage, Is.EqualTo(result));
         }
     }
 }
